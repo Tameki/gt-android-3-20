9b8d48122a8e913f4e56a12bd2db0c5ca1a07930
tameki <myrsabekov@gmail.com>
2020-06-25 21:28:40 +0600

    Add retrofit

--- app/src/main/java/com/geektech/boredapp_20/App.java
+++ app/src/main/java/com/geektech/boredapp_20/App.java
@@ -3,16 +3,19 @@ package com.geektech.boredapp_20;
 import android.app.Application;
 
 import com.geektech.boredapp_20.data.AppPreferences;
+import com.geektech.boredapp_20.data.BoredApiClient;
 
 public class App extends Application {
 
     public static AppPreferences appPreferences;
+    public static BoredApiClient boredApiClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         appPreferences = new AppPreferences(this);
+        boredApiClient = new BoredApiClient();
     }
 
 }
--- app/src/main/java/com/geektech/boredapp_20/presentation/main/MainActivity.java
+++ app/src/main/java/com/geektech/boredapp_20/presentation/main/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.geektech.boredapp_20.App;
 import com.geektech.boredapp_20.R;
+import com.geektech.boredapp_20.data.BoredApiClient;
+import com.geektech.boredapp_20.model.BoredAction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        App.boredApiClient.getAction(null, null, new BoredApiClient.BoredActionCallback() {
+            @Override
+            public void onSuccess(BoredAction boredAction) {
+                Log.d("ololo", boredAction.toString());
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                Log.d("ololo", exception.getMessage());
+            }
+        });
     }
 }
